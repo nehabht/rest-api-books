630f8b87f592fc6f236ed66b26454a132a14d9d7
Neha <ne.bhatti2@icloud.com>
2024-02-01 01:41:17 +0100

    feat: Add endpoint to retrieve a book by ISBN
    Implement a new endpoint to handle HTTP GET requests for retrieving a book based on its ISBN. The method uses the BookService to find the book, and if found, returns a ResponseEntity with the book and HTTP status OK. If the book is not found, it returns a ResponseEntity with HTTP status NOT_FOUND.

--- src/main/java/com/example/books/controllers/BookController.java
+++ src/main/java/com/example/books/controllers/BookController.java
@@ -1,9 +1,12 @@
 package com.example.books.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -49,4 +52,26 @@ public class BookController {
             return response;
 
     }
+
+
+    /**
+     * Handles the HTTP GET request to retrieve a book based on its ISBN.
+     *
+     * @param isbn The ISBN of the book to be retrieved.
+     * @return ResponseEntity with the retrieved Book and HTTP status OK if found,
+     *         or a ResponseEntity with HTTP status NOT_FOUND if the book is not found.
+     */
+    @GetMapping(path = "/books/{isbn}")
+    public ResponseEntity<Book> retriveBook(@PathVariable final String isbn){
+
+        // Retrieve a book from the service using the provided ISBN
+        final Optional<Book> foundBook = bookService.findById(isbn); 
+
+        // If the book is found, return a ResponseEntity with the book and HTTP status OK
+        // Otherwise, return a ResponseEntity with HTTP status NOT_FOUND
+        return foundBook.map(book -> new ResponseEntity<Book>(book,HttpStatus.OK))
+            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+    }
+
 }
